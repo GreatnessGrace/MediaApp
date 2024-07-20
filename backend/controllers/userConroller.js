b3917748ffcb9d92c90f0f3ac85daa3ebaadde65
GreatnessGrace <graceunstoppable7@gmail.com>
2024-07-20 19:39:52 +0530

    login

--- backend/controllers/userConroller.js
+++ backend/controllers/userConroller.js
@@ -55,3 +55,26 @@ exports.createUser = async (req, res) => {
     res.status(500).json({ error: "Error creating user" });
   }
 };
+
+exports.loginUser = async (req, res) => {
+  const { firstName, password } = req.body;
+
+  try {
+    const user = await User.findOne({ firstName });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: "Error logging in" });
+  }
+};
